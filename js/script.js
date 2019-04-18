4380d47fb13f13f42742b96e1a4c2ca5ad62801a
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-04-18 19:53:40 +0200

    Add const

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-let url = 'https://api.icndb.com/jokes/random';
-let button = document.getElementById('get-joke');
+const url = 'https://api.icndb.com/jokes/random';
+const button = document.getElementById('get-joke');
 button.addEventListener('click', () => getJoke());
-let paragraph = document.getElementById('joke');
+const paragraph = document.getElementById('joke');
 function getJoke() {
-    let xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.addEventListener('load', () => {
-        let response = JSON.parse(xhr.response);
+        const response = JSON.parse(xhr.response);
         paragraph.innerHTML = response.value.joke;
     });
     xhr.send();
